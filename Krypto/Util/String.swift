bb4a8e47a6007340c9cd5d6ddbc56dff400a34d2
Mohamed Saber <mohamedsaber09@gmail.com>
2024-04-11 07:13:53 +0200

    Add percentage view

---
+++ Krypto/Util/String.swift
@@ -0,0 +1,8 @@
+//
+//  String.swift
+//  Krypto
+//
+//  Created by Mohamed Saber on 11/04/2024.
+//
+
+import Foundation
---
+++ Krypto/core/Home/View/AssetHorizontalItem.swift
@@ -0,0 +1,18 @@
+//
+//  AssetHorizontalItem.swift
+//  Krypto
+//
+//  Created by Mohamed Saber on 11/04/2024.
+//
+
+import SwiftUI
+
+struct AssetHorizontalItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AssetHorizontalItem()
+}
