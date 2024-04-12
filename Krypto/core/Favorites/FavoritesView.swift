3fae60eae871a76d4f81957dd703336117e4a3c6
Mohamed Saber <mohamedsaber09@gmail.com>
2024-04-12 14:34:22 +0200

    Add ability to delete asset

--- Krypto/core/Favorites/FavoritesView.swift
+++ Krypto/core/Favorites/FavoritesView.swift
@@ -12,17 +12,27 @@ struct FavoritesView: View {
     
     @Query var favorites : [FavoriteAsset]
     
+    @Environment(\.modelContext) private var modelContext
+
+    
     
     var body: some View {
         List{
             Text("Favorites").font(.title) .bold()
 
             ForEach(favorites){ favorites in
-                AssetItem(asset: Asset(id: favorites.id, rank: favorites.rank, symbol: favorites.symbol, name: favorites.name, supply: favorites.supply, maxSupply: favorites.maxSupply, marketCapUsd: favorites.marketCapUsd, volumeUsd24Hr: favorites.volumeUsd24Hr, priceUsd: favorites.priceUsd, changePercent24Hr: favorites.changePercent24Hr, vwap24Hr: favorites.vwap24Hr))
+                let favoriteAsset = Asset(id: favorites.id, rank: favorites.rank, symbol: favorites.symbol, name: favorites.name, supply: favorites.supply, maxSupply: favorites.maxSupply, marketCapUsd: favorites.marketCapUsd, volumeUsd24Hr: favorites.volumeUsd24Hr, priceUsd: favorites.priceUsd, changePercent24Hr: favorites.changePercent24Hr, vwap24Hr: favorites.vwap24Hr)
+                AssetItem(asset:favoriteAsset ).swipeActions{
+                    Button("Delete"){
+                        modelContext.delete( favorites)
+                    }.tint(Color.red)
+
+                }
             }
+        }.listStyle(InsetListStyle())
         }
     }
-}
+    
 
 #Preview {
     FavoritesView()
